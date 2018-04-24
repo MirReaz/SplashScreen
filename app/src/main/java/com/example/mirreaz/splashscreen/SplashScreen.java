4797200367ffd6ee5eb12acb8e0218ddd7c7e1c6
MirReaz <reaz.ctg365@live.com>
2018-04-25 02:38:39 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mirreaz/splashscreen/SplashScreen.java
@@ -0,0 +1,55 @@
+package com.example.mirreaz.splashscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+        */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+
+    }}
+
+
